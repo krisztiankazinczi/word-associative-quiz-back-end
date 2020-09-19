e7cc526c27840809b97625a9303bad69c145be04
Krisz <krisz@example.com>
2020-09-19 15:55:48 +0200

    limit decreased

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const corsOptions = {
 
 app.use(cors());
 
-app.use(express.json({ limit: "10kb" }));
+// app.use(express.json({ limit: "10kb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //API ROUTES
